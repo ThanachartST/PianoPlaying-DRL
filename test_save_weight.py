b41089c0182e466548487c69e25763efd060b5bb
ThanachartST <thanachart.st@gmail.com>
2024-05-01 22:11:54 +0700

    Test for saving torch weight

---
+++ test_save_weight.py
@@ -0,0 +1,34 @@
+from train import get_env, Args
+from algorithm.DroQSAC import DroQSACAgent, DroQSACConfig
+from common.EnvironmentSpec import EnvironmentSpec
+import tyro
+import torch
+
+
+def main(args: Args) -> None:
+    env = get_env(Args)
+    spec = EnvironmentSpec.make(env)
+    agent = DroQSACAgent(spec=spec,
+                         config=args.agent_config,
+                         gamma=args.discount)
+    before_remove_statedict = agent.policy_net.state_dict()
+    
+    torch.save( agent, './test.pt' )
+    del agent
+    try:
+        print( agent.policy_net.state_dict() )
+    except:
+        print( f'agent has been delete' )
+
+    agent: DroQSACAgent = torch.load( './test.pt' )
+    # print( agent.policy_net.state_dict() )
+    after_remove_statedict = agent.policy_net.state_dict()
+
+    for before_val, after_val in zip( before_remove_statedict.values(), after_remove_statedict.values() ):
+        if not torch.equal( before_val, after_val ):
+            raise ValueError
+
+
+
+if __name__ == "__main__":
+    main(tyro.cli(Args, description=__doc__))
