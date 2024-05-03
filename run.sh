e4332c57700259eac9fe3db95d798501154492d2
ThanachartST <thanachart.st@gmail.com>
2024-05-03 14:08:51 +0700

    Add argument batch size

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ export PYTHONPATH="$PWD/robopianist"
 
 WANDB_DIR=/tmp/robopianist/ MUJOCO_GL=egl XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=0 MUJOCO_EGL_DEVICE_ID=0 python train.py \
     --root-dir /tmp/robopianist/rl/ \
+    --batch-size 512\
     --warmup-steps 5000 \
-    --total-steps 300000 \
+    --total-steps 1_000_000 \
     --discount 0.8 \
     --agent-config.critic-dropout-rate 0.01 \
     --agent-config.critic-layer-norm \
@@ -15,10 +16,10 @@ WANDB_DIR=/tmp/robopianist/ MUJOCO_GL=egl XLA_PYTHON_CLIENT_PREALLOCATE=false CU
     --reduced-action-space \
     --control-timestep 0.05 \
     --n-steps-lookahead 10 \
-    --environment-name "RoboPianist-debug-TwinkleTwinkleRousseau-v0" \
+    --environment-name "RoboPianist-clair_de_lune" \
     --action-reward-observation \
     --primitive-fingertip-collisions \
     --eval-episodes 1 \
     --camera-id "piano/back" \
-    # --midi-path "$PWD/robopianist/robopianist/music/data/pig_single_finger/clair_de_lune-1.proto"
+    --midi-path "$PWD/robopianist/robopianist/music/data/pig_single_finger/clair_de_lune-1.proto"
     # --name ""
